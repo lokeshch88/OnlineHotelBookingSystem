93edcdaa47e6a6ba57585795f55aeba7d7197671
lokeshch88 <lokeshchaudhari.7083@gmail.com>
2023-12-14 02:05:52 +0530

    first

---
+++ hotel-management-system-frontend/src/page/ContactUs.js
@@ -0,0 +1,26 @@
+const ContactUs = () => {
+  return (
+    <div className="text-color ms-5 me-5 mr-5 mt-3">
+      <b>
+        Lokesh Chaudhari 
+        8888415848
+
+
+      </b>
+
+      <p>
+        <b>
+           Mohit Chaudhari
+        9567982873
+
+        </b>
+       
+
+      </p>
+    </div>
+
+    
+  );
+};
+
+export default ContactUs;
